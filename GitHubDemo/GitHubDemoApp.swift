ff368f066cb71ac99eb1c35839b9e6bed063e12a
GuruMahan <iosdeve2022@gmail.com>
2023-02-02 18:52:11 +0530

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Guru Mahan on 02/02/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
